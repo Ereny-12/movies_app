c0258cf2d92aa2aea5adf01d05932de2baa1933d
Ereny-12 <164355446+Ereny-12@users.noreply.github.com>
2026-04-23 20:32:28 +0200

    finish onboarding ui finish login screen ui add register page design ui: design forget password screen implement a simple static movie home page add search screen ui ui: add update profile screen

--- lib/features/profile/update_profile.dart
+++ lib/features/profile/update_profile.dart
@@ -12,6 +12,7 @@ class UpdateProfile extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: ColorsManager.black,
       appBar:_buildAppBar (),
